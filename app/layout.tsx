a8cfdb67f0561d3a280726ba00adb875933f4c24
1040342522@qq.com <1040342522@qq.com>
2023-06-06 17:53:20 +0800

    更新标签

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "车之谷&ChatGPT",
+  description: "车之谷养车,为智能而生.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "车之谷&ChatGPT",
     statusBarStyle: "default",
   },
 };
